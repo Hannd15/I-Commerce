de463cb139d4630de8507bd53e9d4f245df93eec
Hannd15 <lejhernandez08gmail.com>
2024-10-26 15:10:05 -0500

    Added tailwind

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,22 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Type::factory()->create([
+            'name' => 'Test Type 1',
+        ]);
+        Type::factory()->create([
+            'name' => 'Test Type 2',
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'username' => 'User1',
+            'password' => Hash::make('password1'),
+            'id_type' => 1,
+        ]);
+        User::factory()->create([
+            'username' => 'User2',
+            'password' => Hash::make('password2'),
+            'id_type' => 2,
         ]);
     }
 }
