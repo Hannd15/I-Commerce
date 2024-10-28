3208a4d0a9f28133c8ebdff072770022f40e0bd5
Vinnnyh <116193876+Vinnnyh@users.noreply.github.com>
2024-10-27 20:43:42 -0500

    desarrollo interfaces 1
    login & register

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,28 +1,20 @@
-<nav class="bg-gray-800 text-white">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16">
-            <!-- Logo or Brand Name -->
-            <div class="flex-shrink-0">
-                <a href="{{ route('home') }}" class="text-2xl font-bold text-white">
-                    I-Commerce
-                </a>
-            </div>
-            <!-- Navigation Links -->
-            <div class="hidden md:block">
-                <div class="ml-10 flex space-x-4 align-middle">
-                    <a href="{{ route('home') }}"
-                       class="{{ request()->routeIs('home') ? 'text-blue-400' : 'text-gray-300' }} hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                        Home
-                    </a>
-                    <a href="{{ route('cart') }}"
-                       class="{{ request()->routeIs('services') ? 'text-blue-400' : 'text-gray-300' }} hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                       Cart
-                    </a>
-                    <a href="{{ route('xd') }}">
-                        <img src="https://i.pinimg.com/736x/95/d2/ca/95d2caa85fd7870a892638d995630396.jpg" alt="" class="w-10 h-10 rounded-full">
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</nav>
+<nav class="nav">
+    <a href="{{ route('home') }}" class="nav-item" active-color="#066057">
+        Home
+    </a>
+
+    <a class="nav-item" active-color="#328068">
+        Profile
+    </a>
+    
+    <a class="nav-item" active-color="#4bc19f">
+        Icomerce
+    </a>
+
+    <a href="{{ route('cart') }}" class="nav-item" active-color="#cad1a5">
+        Cart
+    </a>
+                        
+    <span class="nav-indicator"></span>                
+</nav>    
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    Se hicieron los momos en video
-    <form action="{{ route('logout') }}" method="get">
-        <button type="submit">Logout</button>
-    </form>
+    <section class="header">
+        <img src="{{ asset('images/I_COOMERCE2.png') }}">
+    </section>
+    <section class="about">
+        <h2 class="title">What is?</h2>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus, metus sed accumsan fermentum, orci nisl luctus purus, eget bibendum justo lorem eget nibh. Nulla facilisi. 
+            Aliquam erat volutpat, ut vehicula sapien."
+        </p>
+    </section>
 @endsection
